165cb0670f6c35b96375af4a571bed6c6cc898b0
Mathias Geat <mathias@ailoo.net>
2009-01-21 18:04:30 +0100

    add AboutWindow

---
+++ TouchlessViewer/AboutWindow.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace TouchlessViewer
+{
+    partial class AboutWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutWindow));
+            this.aboutBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // aboutBox
+            // 
+            this.aboutBox.Location = new System.Drawing.Point(13, 13);
+            this.aboutBox.Multiline = true;
+            this.aboutBox.Name = "aboutBox";
+            this.aboutBox.Size = new System.Drawing.Size(259, 239);
+            this.aboutBox.TabIndex = 0;
+            this.aboutBox.Text = resources.GetString("aboutBox.Text");
+            // 
+            // AboutWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 264);
+            this.Controls.Add(this.aboutBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "AboutWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "About TouchLessViewer";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox aboutBox;
+
+    }
+}
