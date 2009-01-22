c854946f2da0a0e33a84099c0abc029a0affd199
Mathias Geat <mathias@ailoo.net>
2009-01-22 19:55:29 +0100

    add TouchlessManager class

---
+++ TouchlessViewer/Touchless/TouchlessManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing.Imaging;
+using System.Drawing;
+using TouchlessLib;
+
+namespace TouchlessViewer
+{
+    class TouchlessManager
+    {
+        protected bool _addingMarker = false;
+        protected static float _markerRadius;
+        protected static Point _markerCenter;
+        protected static Marker _markerSelected;
+        protected static int _addedMarkerCount = 0;
+        
+        protected TouchlessMgr _touchless;
+        public TouchlessMgr Touchless
+        {
+            get { return this._touchless; }
+            set { this._touchless = value; }
+        }
+
+        public TouchlessManager()
+        {
+            this.Touchless = new TouchlessMgr();
+        }
+    }
+}
